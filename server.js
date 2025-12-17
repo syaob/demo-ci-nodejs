03e892e41f9d43e3536d255e6ca66d7f902ecb0e
saikou balde <syaob@yahoo.fr>
2025-12-17 12:29:49 +0100

    une modification server pour tester le fonction du cd notomment l'option watch de pm2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World !");
+  res.send("Hello World MAJ DE L'API!");
 });
 
 app.get("/status", (req, res) => {
