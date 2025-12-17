49b00051b11798f1dc3b910e5c2ddfabb85d088d
saikou balde <syaob@yahoo.fr>
2025-12-17 12:16:05 +0100

    ajout server pour tester la mise prod - ajout script poseidon pour lancement server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World !");
+});
+
+app.get("/status", (req, res) => {
+  res.json({ status: "API is running" });
+});
+
+app.get("/date", (req, res) => {
+  const currentDate = new Date();
+  res.send(`Current date and time is: ${currentDate}`);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
